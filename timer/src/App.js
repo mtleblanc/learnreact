dc08c2718e36bba88453ac429a8713e5ac6e1949
Michael LeBlanc <leblanc.michael@gmail.com>
2018-11-13 16:46:30 -0500

    Add timer editing, creation and deletion

--- timer/src/App.js
+++ timer/src/App.js
@@ -10,12 +10,62 @@ class App extends Component {
 }
 
 class TimerDashboard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      timers: [
+        {
+          id: 1,
+          title: "Timer 1",
+          project: "Watch Twitch",
+          elapsed: 278342,
+        },
+        {
+          id: 2,
+          title: "Timer 2",
+          project: "Learn React",
+          elapsed: 6464124,
+        },
+        {
+          id: 3,
+          title: "Timer 3",
+          project: "Recite Judge Dredd from memory",
+          elapsed: 99999999999,
+        },
+      ]
+    }
+    this.updateTimer = this.updateTimer.bind(this);
+    this.addTimer = this.addTimer.bind(this);
+    this.deleteTimer = this.deleteTimer.bind(this);
+  }
+
+  updateTimer(timer) {
+    const newTimers = this.state.timers.map(t=>
+      t.id === timer.id ? timer : t);
+    this.setState({timers: newTimers});
+  }
+
+  addTimer(timer) {
+    const newId = Math.max.apply(null, this.state.timers.map(t=>t.id).concat(-1))+ 1;
+    const newTimers = this.state.timers.concat(Object.assign({}, timer, {id: newId, elapsed: 0}));
+    this.setState({timers: newTimers});
+  }
+
+  deleteTimer(id) {
+    const newTimers = this.state.timers.filter(t => t.id !== id);
+    this.setState({timers: newTimers});
+  }
+
   render() {
     return (
       <div className='ui three column centered grid'>
         <div className='column'>
-          <TimerList />
-          <TimerForm isOpen={false} />
+          <TimerList 
+            timers={this.state.timers} 
+            updateFunc={this.updateTimer}
+            deleteFunc={this.deleteTimer}
+          />
+          <TimerForm isOpen={false} updateFunc={this.addTimer} />
         </div>
       </div>
       );
@@ -24,7 +74,16 @@ class TimerDashboard extends Component {
 
 class TimerList extends Component {
   render() {
-    return ["#ff0000", "#00ff00", "#0000ff"].map((c,k) => <Timer key={c} title={c} project={k} editing={k%2 === 0} />);
+    return this.props.timers.map(t=>
+      <Timer 
+        key={t.id}
+        id={t.id}
+        title={t.title}
+        project={t.project}
+        elapsed={t.elapsed}
+        updateFunc={this.props.updateFunc}
+        deleteFunc={this.props.deleteFunc}
+      />);
   }
 }
 
@@ -45,7 +104,7 @@ class TimerForm extends Component {
   }
   render() {
     if(this.state.isOpen)
-      return <TimerEditor closeFunc={this.closeForm} />;
+      return <TimerEditor closeFunc={this.closeForm} updateFunc={this.props.updateFunc}/>;
     else
       return (
         <div className='ui basic content center aligned segment'>
@@ -74,18 +133,55 @@ class Timer extends Component {
   }
   render() {
     if(this.state.isOpen)
-      return <TimerEditor title={this.props.title} project={this.props.project} closeFunc={this.closeForm} />;
+      return <TimerEditor 
+        id={this.props.id}
+        title={this.props.title}
+        project={this.props.project} 
+        elapsed={this.props.elapsed}
+        closeFunc={this.closeForm} 
+        updateFunc={this.props.updateFunc} />;
     else
-      return <TimerDisplay 
+      return <TimerDisplay
+                id={this.props.id}
                 title={this.props.title}
                 project={this.props.project}
-                elapsed={209382}
+                elapsed={this.props.elapsed}
                 editFunc={this.openForm}
+                deleteFunc={this.props.deleteFunc}
               />;
   }
 }
 
 class TimerEditor extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: this.props.title || '',
+      project: this.props.project || '',
+    }
+    this.updateTitle = this.updateTitle.bind(this);
+    this.updateTimer = this.updateTimer.bind(this);
+    this.updateProject = this.updateProject.bind(this);
+  }
+
+  updateTimer() {
+    this.props.updateFunc({
+      id: this.props.id,
+      title: this.state.title,
+      project: this.state.project,
+      elapsed: this.props.elapsed,
+    });
+    this.props.closeFunc();
+  }
+
+  updateTitle(evt) {
+    this.setState({title: evt.target.value || ''});
+  }
+
+  updateProject(evt) {
+    this.setState({project: evt.target.value || ''});
+  }
+
   render() {
     const submitText = this.props.title ? 'Update' : 'Create';
     return (
@@ -94,14 +190,14 @@ class TimerEditor extends Component {
           <div className='ui form'>
             <div className='field'>
               <label>Title</label>
-              <input type='text' defaultValue={this.props.title} />
+              <input type='text' defaultValue={this.props.title} onChange={this.updateTitle} />
             </div>
             <div className='field'>
               <label>Project</label>
-              <input type='text' defaultValue={this.props.project} />
+              <input type='text' defaultValue={this.props.project} onChange={this.updateProject}/>
             </div>
             <div className='ui two bottom attached buttons'>
-              <button className='ui basic blue button' onClick={this.props.closeFunc}>
+              <button className='ui basic blue button' onClick={this.updateTimer}>
                 {submitText}
               </button>
               <button className='ui basic button red' onClick={this.props.closeFunc}>
@@ -116,6 +212,15 @@ class TimerEditor extends Component {
 }
 
 class TimerDisplay extends Component {
+  constructor(props) {
+    super(props);
+    this.deleteThis = this.deleteThis.bind(this);
+  }
+
+  deleteThis() {
+    this.props.deleteFunc(this.props.id);
+  }
+
   render() {
     const elapsedString = helpers.renderTime(this.props.elapsed);
     return (
@@ -136,7 +241,7 @@ class TimerDisplay extends Component {
             <span className='right floated edit icon' onClick={this.props.editFunc}>
               <i className='edit icon' />
             </span>
-            <span className='right floated trash icon'>
+            <span className='right floated trash icon' onClick={this.deleteThis}>
               <i className='trash icon' />
             </span>
           </div>
